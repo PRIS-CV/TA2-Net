53122b779394e7de5754f148aef573bb8ea86a44
gyr0104 <72775223+gyr0104@users.noreply.github.com>
2023-07-19 10:40:44 +0800

    Update meta_train_resnet18_TA2_Net.sh

--- scripts/meta_train_resnet18_TA2_Net.sh
+++ scripts/meta_train_resnet18_TA2_Net.sh
@@ -3,5 +3,5 @@
 #CUDA_VISIBLE_DEVICES=0 python test_extractor_tsa.py --model.name=imagenet-net --model.dir ./saved_results/sdl \
 #--test.tsa-ad-type residual --test.tsa-ad-form matrix --test.tsa-opt alpha --test.tsa-init eye --test.mode sdl --test.type 5shot
 CUDA_VISIBLE_DEVICES=2 python main.py --model.name=url --model.dir ./saved_results/url \
- --test.mode mdl --test.type 5shot
+ --test.mode mdl --test.type standard
 
